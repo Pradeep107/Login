b4b9a4b1195e4e0bb43e0d8ddc31732f39582253
Pradeep107 <31689606+Pradeep107@users.noreply.github.com>
2018-02-22 21:26:59 +0530

    updated

--- src/com/notes/Login.java
+++ src/com/notes/Login.java
@@ -1,10 +1,17 @@
 package com.notes;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.jasper.tagplugins.jstl.core.Out;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -14,67 +21,49 @@ import com.google.appengine.api.datastore.Query;
 public class Login extends HttpServlet
 {
    
-protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+{
 	DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+	PrintWriter out = resp.getWriter();
 	 String un=req.getParameter("username");
 		String pw=req.getParameter("password");
-//		String myname;
 	Entity e = new Entity("person" );
-//	e.getProperties();
-//	System.out.println(e.getProperties().toString());
-//	e.setProperty("name", un );
-//	e.setProperty("password", pw );
-//	ds.put(e);
-//	myname = (String) e.getProperty("name");
-	
-	
-	Query q = new Query("person");
+    Query q = new Query("person");
 	PreparedQuery pq = ds.prepare(q); 
-			for (Entity result : pq.asIterable()) { 
+			for (Entity result : pq.asIterable()) 
+			{ 
 			String firstName = (String) result.getProperty("name"); 
 			String password = (String) result.getProperty("password"); 
+			try {
 			if(un.equals(firstName))
 			{
+				
 				if(pw.equals(password))
 				{
-					resp.sendRedirect("success.html");
+					HttpSession session = req.getSession();
+					session.setAttribute("user1", un);
+					RequestDispatcher rd = req.getRequestDispatcher("second");
+					rd.forward(req, resp);
 				}
 				else
 				{
-					resp.sendRedirect("failed.html");
+					RequestDispatcher rd = req.getRequestDispatcher("index.html");
+					rd.include(req, resp);
+					out.println("Sorry Try again Wrong password!!");
 				}
+				
 			}
-			/*else
-			{
-				resp.sendRedirect("failed1.html");
-			}*/
+			} finally {
+			 
 			}
-//	System.out.println(myname);
-	
-	/*if(myname.equals(un))
-	{
-		resp.sendRedirect("index.html");
-	}*/
-//	resp.setContentType("text/plain" );
-//	resp.getWriter().println("Welcome to GAE");
-//   System.out.println("Login is running");
-//
-//   String un=req.getParameter("username");
-//	String pw=req.getParameter("password");
-//	System.out.print(un);
-//	
-//	if(un.equals("admin") && pw.equals("admin"))
-//	{
-//		System.out.println("Login Success");
-//		resp.sendRedirect("success.html");
-//		return;
-//	}
-//	else		
-//	{
-//		System.out.println("Login Failed");
-//
-//		//resp.sendRedirect("error.html");
-//		return;
-//	}
+			}
+			
 }
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	PrintWriter out = resp.getWriter();           
+	RequestDispatcher rd = req.getRequestDispatcher("index.html");
+		rd.include(req, resp);
+		out.println("Please Enter the field before logging in");
+	}
 }
--- src/com/notes/resets.java
+++ src/com/notes/resets.java
@@ -1,11 +1,14 @@
 package com.notes;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
@@ -20,14 +23,24 @@ public class resets extends HttpServlet {
 		 String ke =req.getParameter("re");
 		 Entity e = new Entity("person" );
 		 Query w = new Query("person");
-			PreparedQuery pq = ds.prepare(w); 
+			PreparedQuery pq = ds.prepare(w);
+			PrintWriter out = resp.getWriter();     
+			
 					for (Entity result : pq.asIterable()){
 						String First = (String) result.getProperty("name"); 
+						try {
 						if(uo.equals(First)){
+							HttpSession session = req.getSession();
+							String a= (String) session.getAttribute("user1"); 
+							out.println(a);
 							 result.setProperty( "password" , ke);
 							 ds.put(e);
+							 RequestDispatcher rd = req.getRequestDispatcher("index.html");
+								rd.forward(req, resp);
+							 out.println("Password changed sucessfully");
 						}
-					}
+			}finally {} 
 	}
+	} 
 
 }
